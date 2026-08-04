7224f5c2fb10ed100958de0d1652fb95733db8b3
Sourabh <148969510+SourabhSaharan@users.noreply.github.com>
2026-08-04 13:15:51 +0530

    Update API endpoint for fetching appointments, line 14

--- frontend/src/pages/Appointments.jsx
+++ frontend/src/pages/Appointments.jsx
@@ -11,7 +11,7 @@ function Appointments() {
 
   const fetchAppointments = async () => {
     try {
-      const res = await axios.get("http://127.0.0.1:5000/api/appointments");
+      const res = await axios.get("https://disease-backend-hn48.onrender.com/api/appointments");
       setAppointments(res.data);
     } catch (err) {
       console.error("Error fetching appointments:", err);
